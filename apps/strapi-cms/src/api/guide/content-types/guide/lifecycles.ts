0b33c84238fd6eae280ff00e70cac5eb5e52ccbb
Your Name <youremail@yourdomain.com>
2025-11-05 14:37:36 +0100

    Implement immutability check for dirName and set oldDirName on create

--- apps/strapi-cms/src/api/guide/content-types/guide/lifecycles.ts
+++ apps/strapi-cms/src/api/guide/content-types/guide/lifecycles.ts
@@ -49,12 +49,68 @@ const validateGuideVersions = async (event: IGuideEvent) => {
   return true;
 };
 
+const validateDirNameImmutability = async (event: IGuideEvent) => {
+  const guideFromDb = await strapi.db.query('api::guide.guide').findOne({
+    where: { id: event.params.where?.id || '' },
+    populate: { versions: true },
+  });
+
+  console.log(`guideFromDb: ${JSON.stringify(guideFromDb)}`);
+
+  const versions = guideFromDb?.versions || [];
+
+  for (const version of versions) {
+    console.log('version', version)
+    if(version.dirName !== version.oldDirName) {
+      console.log(`dirName changed from '${version.oldDirName}' to '${version.dirName}' error`);
+
+      // Revert the change in the database
+      await strapi.db.connection
+        .from('components_common_guide_versions')
+        .where({ id: version.id })
+        .update({ dir_name: version.oldDirName });
+
+      throw new errors.ApplicationError(
+        `The dirName cannot be changed from '${version.oldDirName}' to '${version.dirName}': the field is immutable.`
+      );
+    }
+  }
+};
+
+// On create, set oldDirName to the initial dirName
+const setOldDirNameOnCreate = async (event: IGuideEvent) => {
+  if (event.params.data.versions && Array.isArray(event.params.data.versions) && event.params.data.versions.length > 0) {
+    const versionIds = event.params.data.versions.map((v) => v.id);
+
+    console.log(`Setting oldDirName for versions: ${JSON.stringify(versionIds)}`);
+
+    // Fetch the full version data
+    const versions = await strapi.db.connection
+      .select('*')
+      .from(`components_common_guide_versions`)
+      .whereIn('id', versionIds);
+
+    console.log(`Fetched versions: ${JSON.stringify(versions)}`);
+
+    await Promise.all(versions.map(async (version) => {
+      version.oldDirName = version.dir_name;
+      // save the change back to the database
+      await strapi.db.connection
+        .from('components_common_guide_versions')
+        .where({ id: version.id })
+        .update({ old_dir_name: version.oldDirName });
+    }));
+  }
+}
+
 module.exports = {
   async beforeCreate(event: IGuideEvent) {
     await validateGuideVersions(event);
+    await setOldDirNameOnCreate(event);
   },
   async beforeUpdate(event: IGuideEvent) {
     await validateGuideVersions(event);
+    await validateDirNameImmutability(event);
   },
   async afterUpdate(event: IGuideEvent) {
     if (event.params.data.publishedAt === undefined) {
