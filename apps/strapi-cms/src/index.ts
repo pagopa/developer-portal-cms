caf25464333acbebc993d493160fe75f904b67f7
Marcello Bertoli <marcello.bertoli@uqido.com>
2025-11-18 15:04:18 +0100

    refactor from switch to array check

--- apps/strapi-cms/src/index.ts
+++ apps/strapi-cms/src/index.ts
@@ -3,27 +3,27 @@ import {
   validateAssociatedProductPresenceOnUpdate
 } from "./utils/validateProductPresence";
 
+const entitiesRequiringProductAssociation = [
+  'api::use-case-list-page.use-case-list-page',
+  'api::use-case.use-case',
+  'api::tutorial-list-page.tutorial-list-page',
+  'api::tutorial.tutorial',
+  'api::quickstart-guide.quickstart-guide',
+  'api::overview.overview',
+  'api::guide-list-page.guide-list-page',
+  'api::api-data-list-page.api-data-list-page',
+  'api::api-data.api-data'];
+
 export default {
   // @ts-ignore
-  register: ({ strapi }) => {
+  register: ({strapi}) => {
     // @ts-ignore
-    strapi.documents.use(async (context,next)=> {
-      switch (context.uid){
-        case 'api::use-case-list-page.use-case-list-page':
-        case 'api::use-case.use-case':
-        case 'api::tutorial-list-page.tutorial-list-page':
-        case 'api::tutorial.tutorial':
-        case 'api::quickstart-guide.quickstart-guide':
-        case 'api::overview.overview':
-        case 'api::guide-list-page.guide-list-page':
-        case 'api::api-data-list-page.api-data-list-page':
-        case 'api::api-data.api-data': {
-          if(context.action === 'create'){
-            validateAssociatedProductPresenceOnCreate(context);
-          }
-          else if(context.action === 'update'){
-            validateAssociatedProductPresenceOnUpdate(context);
-          }
+    strapi.documents.use(async (context, next) => {
+      if (entitiesRequiringProductAssociation.includes(context.uid)) {
+        if (context.action === 'create') {
+          validateAssociatedProductPresenceOnCreate(context);
+        } else if (context.action === 'update') {
+          validateAssociatedProductPresenceOnUpdate(context);
         }
       }
       return next();
