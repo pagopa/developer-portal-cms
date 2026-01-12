c7381575569838911044a1337041cdf504c7390c
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-01-12 14:36:14 +0100

    add provider local to fix image upload

--- apps/strapi-cms/config/plugins.ts
+++ apps/strapi-cms/config/plugins.ts
@@ -1,20 +1,7 @@
 export default ({ env }: any) => ({
   upload: {
     config: {
-      provider: env('UPLOAD_PLUGIN_PROVIDER', 'aws-s3'),
-      providerOptions: {
-        baseUrl: env('CDN_URL'),
-        s3Options: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          endpoint: env('AWS_BUCKET_ENDPOINT'),
-          params: {
-            ACL: 'private',
-            signedUrlExpires: 15 * 60,
-            Bucket: env('AWS_BUCKET_NAME'),
-          },
-        }
-      },
+      provider: 'local',
     },
   },
   'strapi-plugin-sso': {
