dbf7937146153025c9bcd10bef90b02691841e92
Marco Bottaro <marco.bottaro@uqido.com>
2026-03-31 17:20:15 +0200

    Fix import statement formatting in custom-messages-map controller

--- apps/strapi-cms/src/api/custom-messages-map/controllers/custom-messages-map.ts
+++ apps/strapi-cms/src/api/custom-messages-map/controllers/custom-messages-map.ts
@@ -2,6 +2,6 @@
  * custom-messages-map controller
  */
 
-import { factories } from '@strapi/strapi'
+import { factories } from '@strapi/strapi';
 
 export default factories.createCoreController('api::custom-messages-map.custom-messages-map');
