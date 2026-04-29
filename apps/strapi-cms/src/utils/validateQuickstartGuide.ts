72502ec15a080b4e2fd326f4c1ac7a81387a051b
Marcello Bertoli <marcello.bertoli@uqido.com>
2026-04-29 14:49:29 +0200

    code refactor

--- apps/strapi-cms/src/utils/validateQuickstartGuide.ts
+++ apps/strapi-cms/src/utils/validateQuickstartGuide.ts
@@ -1,19 +1,19 @@
-import {errors} from "@strapi/utils";
+import { errors } from "@strapi/utils";
 
 export interface IQuickstartGuideEvent {
   readonly params: {
     readonly data: {
       readonly documentId?: string;
       readonly quickstartGuideItems?: {
-        readonly connect?: IQuickstartGuideItemsEvent[]
-        readonly disconnect?: IQuickstartGuideItemsEvent[]
-      }
-    }
+        readonly connect?: IQuickstartGuideItemsEvent[];
+        readonly disconnect?: IQuickstartGuideItemsEvent[];
+      };
+    };
     readonly documentId?: string;
-  }
+  };
 }
 
-interface IQuickstartGuideItemsEvent {
+export interface IQuickstartGuideItemsEvent {
   readonly id?: string;
   readonly documentId?: string;
   readonly locale?: string;
@@ -21,35 +21,43 @@ interface IQuickstartGuideItemsEvent {
 }
 
 export const validateQuickstartGuideItemsPresenceOnCreate = (event: IQuickstartGuideEvent) => {
-  if(!event.params.data.quickstartGuideItems || event.params.data.quickstartGuideItems!.connect!.length === 0){
+  const { quickstartGuideItems } = event.params.data;
+
+  if (!quickstartGuideItems?.connect?.length) {
     throw new errors.ApplicationError('QuickstartGuide must have at least one quickstart guide item');
   }
+
   return true;
-}
+};
 
 export const validateQuickstartGuideItemsPresenceOnUpdate = async (event: IQuickstartGuideEvent) => {
   const documentId = event.params.data.documentId || event.params.documentId;
+
   if (!documentId) {
     throw new errors.ApplicationError('QuickstartGuide documentId not found');
   }
+
   const foundQuickstartGuide = await strapi.db
     .query('api::quickstart-guide.quickstart-guide')
     .findOne({
-      where: {documentId},
+      where: { documentId },
       populate: ['quickstartGuideItems'],
     });
 
   if (!foundQuickstartGuide) {
     throw new errors.ApplicationError('QuickstartGuide not found');
   }
-  const quickstartGuideItemsDelta =event.params.data.quickstartGuideItems!.disconnect!.length - event.params.data.quickstartGuideItems!.connect!.length
 
-  if(event.params.data.quickstartGuideItems && event.params.data.quickstartGuideItems.disconnect!.length > 0) {
-    if (foundQuickstartGuide.quickstartGuideItems.length === 0 || foundQuickstartGuide.quickstartGuideItems.length <= quickstartGuideItemsDelta) {
-      throw new errors.ApplicationError('QuickstartGuide must have at least one quickstart guide item');
-    }
+  const items = event.params.data.quickstartGuideItems;
+  const connectLength = items?.connect?.length || 0;
+  const disconnectLength = items?.disconnect?.length || 0;
+  const currentLength = foundQuickstartGuide.quickstartGuideItems?.length || 0;
+
+  const newLength = currentLength - disconnectLength + connectLength;
+
+  if (newLength <= 0) {
+    throw new errors.ApplicationError('QuickstartGuide must have at least one quickstart guide item');
   }
-  else if(foundQuickstartGuide.quickstartGuideItems.length === 0 && quickstartGuideItemsDelta === 0) {throw new errors.ApplicationError('QuickstartGuide must have at least one quickstart guide item');
-  }
+
   return true;
-}
+};
